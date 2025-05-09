99f3d8297b9ffe45595355353a1aa1d7d414438d
dkandjsl <dkandjsl@gmail.com>
2025-05-09 10:28:15 +0900

    feat: motor test

--- rpi5-turret/module_servo_controller.py
+++ rpi5-turret/module_servo_controller.py
@@ -1,14 +1,21 @@
+# 서보 컨트롤러 채널 번호
 CHANNEL_SERVO_XZ = 0
 CHANNEL_SERVO_YZ = 1
+
+# 서보 모터 6221MG의 최소, 최대 펄스 폭
+# 500us ~ 2500us
 SERVO_MIN_PULSE = 500
 SERVO_MAX_PULSE = 2500
 
+# 레이저의 초기 위치 (카메라와의 거리)
 LASER_X = 0
 LASER_Y = 0
 LASER_Z = 0
 
+# 레이저 GPIO 핀 번호
 PIN_LASER = 17
 
+# PID 제어를 위한 비례 상수 (1이면 영향 없음)
 PID_P = 1
 
 import math
@@ -18,20 +25,27 @@ from gpiozero import LED
 
 class Turret:
     def __init__(self):
+        # 서보 컨트롤러 객체 생성
         self.kit = ServoKit(channels=16)
+
+        # xz평면, yz평면 서보 모터 초기화
         self.servo_xz = self.kit.servo[CHANNEL_SERVO_XZ]
         self.servo_yz = self.kit.servo[CHANNEL_SERVO_YZ]
         self.servo_xz.set_pulse_width_range(SERVO_MIN_PULSE, SERVO_MAX_PULSE)
         self.servo_yz.set_pulse_width_range(SERVO_MIN_PULSE, SERVO_MAX_PULSE)
         self.servos = [self.servo_xz, self.servo_yz]
-        self.laser = LED(PIN_LASER)
         self.servo_xz.angle = 0
         self.servo_yz.angle = 180
+        self.servo_xz.isReversed = False
+        self.servo_yz.isReversed = True
+
+        # 레이저 초기화
+        self.laser = LED(PIN_LASER)
 
     def look_at(self, x, y, z):
         angle_xz = self.calculate_angle(x, z)
         angle_yz = self.calculate_angle(y, z)
-                
+        
         self.servo_xz.angle = pid(angle_xz)
         self.servo_yz.angle = pid(180-angle_yz)
 
@@ -61,11 +75,15 @@ turret = Turret()
 
 target_x = -80
 target_y = 0
-target_z = 100
+target_z = 50
+
+while(target_x<100):
+    turret.look_at(target_x, target_y, target_z)
+    time.sleep(0.5)
+    target_x += 20
+    target_y += 20
 
 turret.look_at(target_x, target_y, target_z)
-
-time.sleep(2)
 # -------------------------
 
 turret.off()
--- rpi5-turret/ptc_servo_controller.py
+++ rpi5-turret/ptc_servo_controller.py
@@ -1,9 +1,9 @@
-import time
 from adafruit_servokit import ServoKit
 import adafruit_pca9685
 import board
 import busio
-
+import time
+import random
 
 i2c = busio.I2C(board.SCL, board.SDA)
 pca = adafruit_pca9685.PCA9685(i2c)
@@ -15,20 +15,19 @@ kit = ServoKit(channels=16)
 kit.servo[0].set_pulse_width_range(500, 2500)
 kit.servo[1].set_pulse_width_range(500, 2500)
 
-kit.servo[0].angle = 180
-time.sleep(1)
-kit.servo[0].angle = 150
-time.sleep(1)
-kit.servo[0].angle = 120
-time.sleep(1)
-kit.servo[0].angle = 90
-time.sleep(1)
-kit.servo[0].angle = 60
-time.sleep(1)
-kit.servo[0].angle = 30
-time.sleep(1)
 kit.servo[0].angle = 0
+kit.servo[1].angle = 0
 
+i = 0
+
+while(i<3):
+  i += 1
+  kit.servo[0].angle = random.randint(0, 180)
+  kit.servo[1].angle = random.randint(0, 180)
+  time.sleep(1)
+
+kit.servo[0].angle = 0
+kit.servo[1].angle = 0
 
 # kit.continuous_servo[1].throttle = 1
 # time.sleep(1)
